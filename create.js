207758485e74a9d79a08caefe74a8e9338aaffcd
kbpsystem <koleen.bp@outlook.com>
2020-02-25 12:19:08 +0800

    building create/get and handler functions

--- create.js
+++ create.js
@@ -1,50 +1,57 @@
-import uuid from 'uuid'
-import AWS from 'aws-sdk'
+import uuid from 'uuid';
+import AWS from 'aws-sdk';
+import * as dynamoDbLib from './libs/dynamodb-lib';
+import { success, failure } from './libs/response-lib';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient()
 
-export function main (event, context, callback) {
-    // To do
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-    const data = JSON.parse(event.body)
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: uuid.v4(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
 
-    const parameters = {
-        TableName: process.env.tableName,
+  try {
+    await dynamoDbLib.call('put', params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 
-        Item: {
-            userId: event.requestContext.identity.cognitoIdentityId,
-            noteId: uuid.v4(),
-            content: data.content,
-            attachment: data.attachment,
-            createdAt: Date.now()
-        }
+  dynamoDB.put(params, (error, data) => {
+    // Set response headers to enable CORS
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    };
+
+    // Return status code 500 on error
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify({
+          status: false,
+        }),
+      };
+      callback(null, response);
+      return;
     }
-
-    dynamoDB.put(parameters, (error, data) => {
-        // Set response headers to enable CORS
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": true
-        }
-
-        // Return status code 500 on error
-        if(error) {
-            const response = {
-                statusCode: 500,
-                headers: headers,
-                body: JSON.stringify({
-                    status: false
-                })
-            }
-            callback(null, response)
-            return
-        }
-        // Return status code 200 and add the created item
-        const response = {
-            statusCode: 200,
-            headers: headers,
-            body: JSON.stringify( parameters.Item)
-        }
-        callback(null, response)
-    })
+    // Return status code 200 and add the created item
+    const response = {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
 }
